e8b39373476b10f0414d3af73657a75dc3768037
Michael Wong <michael.wong@dxc.com>
2024-02-20 09:41:54 +0800

    Update dataIngestion.py

--- dataSources/dataIngestion.py
+++ dataSources/dataIngestion.py
@@ -43,8 +43,8 @@ def dataIngestion():
 
     # To create runnable browser
     urL='https://www.google.com'
-    chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-    webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
+    chrome_path="/usr/bin/google-chrome"
+    webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
     webbrowser.get('chrome').open_new_tab(urL)
 
     # Apple

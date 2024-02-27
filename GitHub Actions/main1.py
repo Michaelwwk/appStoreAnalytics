6a8e0f69def2cfc88bd8c5a615166f7fde802720
Michael Wong <michael.wong@dxc.com>
2024-02-28 03:03:12 +0800

    Update main1.py

--- GitHub Actions/main1.py
+++ GitHub Actions/main1.py
@@ -1,9 +1,9 @@
 from pyspark.sql import SparkSession
-from dataSources.dataIngestionApple import dataIngestionApple
-from dataSources.dataIngestionGoogle import dataIngestionGoogle
-from dataWrangling.dataWrangling import dataWrangling
-from models.models import finalizedMLModels
-from dataSources.dateTime import dateTime
+from ..dataSources.dataIngestionApple import dataIngestionApple
+from ..dataSources.dataIngestionGoogle import dataIngestionGoogle
+from ..dataWrangling.dataWrangling import dataWrangling
+from ..models.models import finalizedMLModels
+from ..dataSources.dateTime import dateTime
 
 # Start Spark session
 spark = SparkSession.builder.master("local").appName("appStoreAnalytics").config('spark.ui.port', '4050').getOrCreate()

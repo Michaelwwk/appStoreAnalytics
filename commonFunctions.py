a23e10528bca23929b4fda014f366e60f6d7bfd0
Michael Wong <michael.wong@dxc.com>
2024-03-01 13:29:36 +0800

    Update commonFunctions.py

--- commonFunctions.py
+++ commonFunctions.py
@@ -86,8 +86,8 @@ def to_gbq(dataframe, client, dataSet_tableName, mergeType ='WRITE_APPEND', spar
         folder_path = f"{folder_path}/work/appStoreAnalytics/appStoreAnalytics"
         local_file_path = f"{folder_path}/{dataSet_tableName}.parquet"
 
-        # dataframe.write.parquet("path_to_parquet_file")
-        dataframe.to_parquet(local_file_path)
+        dataframe.write.parquet(local_file_path)
+        # dataframe.to_parquet(local_file_path)
         df = pd.read_parquet(local_file_path)
 
         # # if using parquet to bucket method, add "parquet_file_path = None" into the function's params! Put pandas df chunk under Else statement

6a30112140f4c40951a88d1e5681ec8096f90134
Michael Wong <michael.wong@dxc.com>
2024-03-03 14:27:20 +0800

    Revert "Update main1.py"
    This reverts commit 1a7cba34eae22caf2fb7f98a0ff8a31f12f80935.

--- mainFiles/main1.py
+++ mainFiles/main1.py
@@ -2,14 +2,6 @@ from ..dataSources.deleteRowsAppleGoogle import deleteRowsAppleGoogle
 from ..dataSources.dataIngestionApple import dataIngestionApple
 from ..dataSources.dataIngestionGoogle import dataIngestionGoogle
 
-import os
-# Get the absolute path of the current file (my_main_file.py)
-current_file_path = os.path.abspath(__file__)
-# Get the directory name of the current file (folder containing my_main_file.py)
-project_root = os.path.dirname(current_file_path)
-print(current_file_path)
-print(project_root)
-
 deleteRowsAppleGoogle()
 dataIngestionApple(noOfSlices = 10, subDf = 1)
 dataIngestionGoogle(noOfSlices = 0)

d5d6cb1f321d85e70d1e0a857d7b1b38f92acb69
Michael Wong <michael.wong@dxc.com>
2024-03-03 14:27:05 +0800

    Revert "test using init"
    This reverts commit a5f56bd9c964ab9cc8c72f9e30c0f62d42849a03.

--- mainFiles/main1.py
+++ mainFiles/main1.py
@@ -1,7 +1,16 @@
-from dataSources.deleteRowsAppleGoogle import deleteRowsAppleGoogle
-from dataSources.dataIngestionApple import dataIngestionApple
-from dataSources.dataIngestionGoogle import dataIngestionGoogle
+# from ..dataSources.deleteRowsAppleGoogle import deleteRowsAppleGoogle
+# from ..dataSources.dataIngestionApple import dataIngestionApple
+# from ..dataSources.dataIngestionGoogle import dataIngestionGoogle
 
-deleteRowsAppleGoogle()
-dataIngestionApple(noOfSlices = 10, subDf = 1)
-dataIngestionGoogle(noOfSlices = 0)
+import os
+
+# Get the absolute path of the current file (my_main_file.py)
+current_file_path = os.path.abspath(__file__)
+# Get the directory name of the current file (folder containing my_main_file.py)
+project_root = os.path.dirname(current_file_path)
+print(current_file_path)
+print(project_root)
+
+# deleteRowsAppleGoogle()
+# dataIngestionApple(noOfSlices = 10, subDf = 1)
+# dataIngestionGoogle(noOfSlices = 0)

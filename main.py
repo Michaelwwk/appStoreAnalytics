d70f6c7881aec739458bd396075813f626dc9fdf
Michael Wong <michael.wong@dxc.com>
2024-03-04 10:47:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,17 @@ wranglingMLDateTime_actionNo = 21 # YAML action no. for wrangling, ML, dateTime
 TrainTest_actionNo = 22 # YAML action no. for TrainTest
 maxNoOfYMLActionNo = 22 # Total no. of YAML files
 
+# Configurations
+folder_path = os.getcwd().replace("\\", "/")
+# folder_path = os.path.abspath(os.path.expanduser('~')).replace("\\", "/")
+# folder_path = f"{folder_path}/work/appStoreAnalytics/appStoreAnalytics"
+googleAPI_dict = json.loads(os.environ["GOOGLEAPI"])
+googleAPI_json_path = f"{folder_path}/googleAPI.json"
+with open(googleAPI_json_path, "w") as f:
+    json.dump(googleAPI_dict, f)
+project_id =  googleAPI_dict["project_id"]
+client = bigquery.Client.from_service_account_json(googleAPI_json_path, project = project_id)
+
 ### Data Ingestion ###
 
 currentAppleSubDf = 1
@@ -52,14 +63,6 @@ def wranglingMLDateTime_TrainTest(trainTest = False):
     # Start Spark session
     spark = SparkSession.builder.master("local").appName("appStoreAnalytics").config('spark.ui.port', '4050').getOrCreate()
     if trainTest == False:
-        # Configurations
-        folder_path = os.getcwd().replace("\\", "/")
-        googleAPI_dict = json.loads(os.environ["GOOGLEAPI"])
-        with open("googleAPI.json", "w") as f:
-            json.dump(googleAPI_dict, f)
-        project_id =  googleAPI_dict["project_id"]
-        googleAPI_json_path = f"{folder_path}/googleAPI.json"
-        client = bigquery.Client.from_service_account_json(googleAPI_json_path, project = project_id)
         # Run main functions
         dataWrangling(spark, project_id, client, googleAPI_json_path)
         time.sleep(5)
@@ -71,23 +74,13 @@ def wranglingMLDateTime_TrainTest(trainTest = False):
         except:
             pass
     else:
-        # Set folder path
-        folder_path = os.path.abspath(os.path.expanduser('~')).replace("\\", "/")
-        folder_path = f"{folder_path}/work/appStoreAnalytics/appStoreAnalytics"
-        googleAPI_json_path = f"{folder_path}/googleAPI.json"
-        # Extract Google API from GitHub Secret Variable
-        googleAPI_dict = json.loads(os.environ["GOOGLEAPI"])
-        with open(googleAPI_json_path, "w") as f:
-            json.dump(googleAPI_dict, f)
         # Hard-coded variables
-        project_id =  googleAPI_dict["project_id"]
         rawDataset = "rawData"
         trainTestDataset = "trainTestData"
         AppleScraped_table_name = 'appleMain'
         AppleReview_table_name = 'appleReview'
         GoogleScraped_table_name = 'googleMain'
         GoogleReview_table_name = 'googleReview'
-        client = bigquery.Client.from_service_account_json(googleAPI_json_path, project = project_id)
         table_names = [AppleScraped_table_name, AppleReview_table_name, GoogleScraped_table_name, GoogleReview_table_name]
         for table_name in table_names:
             trainTest_dataSetTableName = f"{trainTestDataset}.{table_name}"
@@ -95,11 +88,6 @@ def wranglingMLDateTime_TrainTest(trainTest = False):
             sparkDf = read_gbq(spark, client, googleAPI_json_path, GBQdataset = rawDataset, GBQtable = table_name)
             client.create_table(bigquery.Table(trainTest_db_path), exists_ok = True)
             to_gbq(sparkDf, client, trainTest_dataSetTableName, mergeType ='WRITE_TRUNCATE', sparkdf = True)
-        ## Remove files and folder
-        try:
-            os.remove(googleAPI_json_path)
-        except:
-            pass
     # Stop Spark session
     spark.stop()
 
@@ -110,9 +98,14 @@ main_dict[wranglingMLDateTime_actionNo] = create_wranglingMLDateTime_TrainTest(t
 main_dict[TrainTest_actionNo] = create_wranglingMLDateTime_TrainTest(trainTest = True)
 
 ### Run above functions conditionally depending on which YAML file is calling it ###
-
 for action_inputNo in range(1, maxNoOfYMLActionNo+1):
     if sys.argv[1] == str(action_inputNo):
         main_dict[action_inputNo]()
     else:
         pass
+
+## Remove files and folder
+try:
+    os.remove(googleAPI_json_path)
+except:
+    pass

25ccc78a7e2a9a399b0ed7fb944b17e7001fe372
Michael Wong <michael.wong@dxc.com>
2024-03-06 14:49:52 +0800

    Update dataIngestionApple.py

--- dataSources/dataIngestionApple.py
+++ dataSources/dataIngestionApple.py
@@ -20,7 +20,7 @@ import logging
 logging.basicConfig(level=logging.ERROR)
 
 # Hard-coded variables
-appleAppsSample = 160 # 999 = all samples!
+appleAppsSample = 1600 # 999 = all samples!
 saveReviews = True
 appleReviewCountPerApp = 20 # max 20!
 requests_per_second = None # None = turn off throttling!

7c689e2b095767634b95631ab3e610e16da871b0
Michael Wong <michael.wong@dxc.com>
2024-03-10 09:39:24 +0800

    Update dataWrangling.py

--- dataWrangling/dataWrangling.py
+++ dataWrangling/dataWrangling.py
@@ -21,4 +21,7 @@ def dataWrangling(spark, project_id, client):
 """
 TODO For both Apple & Google Reviews table, need to sort by appId, user ID, comment ID, date, etc
      in ascending order then take last row [drop duplicates(subset = 'columns without developers' replies', keep = 'last')]!
+
+TODO Since review tables are cumulative and main tables are latest (both Apple & Google), ensure that review tables only contain appIds that are in main tables!
+(Actually is it even a good idea for review tables to be cumulative though? Scared it may exceed the 10GB free tier limit as it's few 100k rows each pull and we doing it daily.)
 """

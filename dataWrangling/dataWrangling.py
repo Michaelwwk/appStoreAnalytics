305510fdf7620332011378f7cb005c1695cac643
yorwel <79965482+yorwel@users.noreply.github.com>
2024-03-21 22:34:47 +0800

    Update dataWrangling.py

--- dataWrangling/dataWrangling.py
+++ dataWrangling/dataWrangling.py
@@ -9,17 +9,6 @@ cleanGoogleScraped_table_name = 'cleanGoogleMain' # TODO CHANGE PATH
 
 # TODO Follow this template when scripting!!
 def dataWrangling(spark, project_id, client):
-
-            # Define schema for the table
-    schema = [
-        bigquery.SchemaField("minInstalls", "INT64"),
-        bigquery.SchemaField("realInstalls", "INT64"),
-        bigquery.SchemaField("score", "INT64"),
-        bigquery.SchemaField("ratings", "INT64"),
-        bigquery.SchemaField("reviews", "INT64"),
-        bigquery.SchemaField("price", "INT64"),
-        # Define other fields here with appropriate data types
-    ]
     
     cleanGoogleScraped_db_path = f"{project_id}.{cleanDataset}.{cleanGoogleScraped_table_name}"
 
@@ -31,20 +20,32 @@ def dataWrangling(spark, project_id, client):
     def clean_data(df):
         # Convert datatype from string to integer for specified columns
         columns_to_convert = ['minInstalls', 'realInstalls', 'score', 'ratings', 'reviews', 'price']
-        for col_name in columns_to_convert:
-            df = df.withColumn(col_name, col(col_name).cast("int"))
+
+        # for col_name in columns_to_convert:
+        #     df = df.withColumn(col_name, col(col_name).cast("int"))
+
+        # Apply cast to integer for specified columns
+        df = (df
+            .select(*[col(col_name).cast("int").alias(col_name) if col_name in columns_to_convert else col(col_name) for col_name in df.columns])
+            )
 
         # Drop specific columns
         columns_to_drop = ['descriptionHTML', 'sale', 'saleTime', 'originalPrice', 'saleText', 'developerId', 'containsAds', 'updated', 'appId']
         df = df.drop(*columns_to_drop)
+        
 
         # Remove specified strings from specified columns
         strings_to_remove = {
             'description': ['<b>']
         }
-        for column, strings in strings_to_remove.items():
-            for string in strings:
-                df = df.withColumn(column, regexp_replace(col(column), string, ""))
+        # for column, strings in strings_to_remove.items():
+        #     for string in strings:
+        #         df = df.withColumn(column, regexp_replace(col(column), string, ""))
+
+        # Apply regexp_replace to remove specified strings from the respective columns
+        df = (df
+            .select(*[regexp_replace(col(column), string, "").alias(column) if column == col_name else col(column) for col_name, strings in strings_to_remove.items() for string in strings])
+            )        
 
         # Drop records where 'ratings' column has a value of 0
         df = df.filter(col("ratings") != 0)
@@ -68,7 +69,39 @@ def dataWrangling(spark, project_id, client):
 
     client.create_table(bigquery.Table(cleanGoogleScraped_db_path, schema=schema), exists_ok = True)
     to_gbq(cleaned_sparkDf, cleanDataset, cleanGoogleScraped_table_name)
+
+
+# Hard-coded variables
+cleanDataset = "cleanData"
+cleanGoogleScraped_table_name = 'cleanGoogleReviews'
+
+# TODO Follow this template when scripting!!
+def dataWrangling(spark, project_id, client):
     
+    cleanGoogleScraped_db_path = f"{project_id}.{cleanDataset}.{cleanGoogleScraped_table_name}"
+
+    sparkDf = read_gbq(spark, rawDataset, googleScraped_table_name)
+    # print(sparkDf.show())
+    print(sparkDf.count())
+
+    # Code section for cleaning googlereview data
+    def clean_data(df):
+
+        # Drop specific columns
+        columns_to_drop = ['userName', 'reviewCreatedVersion', 'appVersion']
+        df = df.drop(*columns_to_drop)
+
+        # Remove duplicates
+        columns = ['reviewId']  # Example columns, modify as needed
+        return df.drop_duplicates(subset=columns)
+    
+
+    
+    cleaned_sparkDf = clean_data(sparkDf)
+
+    client.create_table(bigquery.Table(cleanGoogleScraped_db_path), exists_ok = True)
+    to_gbq(cleaned_sparkDf, cleanDataset, cleanGoogleScraped_table_name)
+
 """
 TODO For both Apple & Google Reviews table, need to sort by appId, user ID, comment ID, date, etc
      in ascending order then take last row [drop duplicates(subset = 'columns without developers' replies', keep = 'last')]!
@@ -76,3 +109,4 @@ TODO For both Apple & Google Reviews table, need to sort by appId, user ID, comm
 TODO Since review tables are cumulative and main tables are latest (both Apple & Google), ensure that review tables only contain appIds that are in main tables!
 (Actually is it even a good idea for review tables to be cumulative though? Scared it may exceed the 10GB free tier limit as it's few 100k rows each pull and we doing it daily.)
 """
+

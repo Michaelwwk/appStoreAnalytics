867a668f036bbe7f923bf0429e0564454b34bb52
Michael Wong <michael.wong@dxc.com>
2024-04-04 13:54:12 +0800

    Update models.py

--- models/models.py
+++ models/models.py
@@ -8,7 +8,10 @@ modelGoogleScraped_table_name = 'modelGoogleMain' # TODO CHANGE PATH
 
 # TODO Follow this template when scripting!!
 def appleClassificationModel(spark, project_id, client):
+    return
 
+def googleClassificationModel(spark, project_id, client):
+    
     modelGgoogleScraped_db_path = f"{project_id}.{modelDataset}.{modelGoogleScraped_table_name}"
 
     sparkDf = read_gbq(spark, cleanDataset, cleanGoogleMainScraped_table_name)
@@ -17,9 +20,6 @@ def appleClassificationModel(spark, project_id, client):
 
     client.create_table(bigquery.Table(modelGgoogleScraped_db_path), exists_ok = True)
     to_gbq(sparkDf, modelDataset, modelGoogleScraped_table_name)
-
-def googleClassificationModel(spark, project_id, client):
-    return
 
 def appleRecommenderModel(spark, project_id, client):
     return

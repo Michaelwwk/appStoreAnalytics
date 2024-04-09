2f324569ae65da8cbee174ae7344bde9da4645b5
Michael Wong <michael.wong@dxc.com>
2024-04-09 15:25:05 +0800

    update

--- models/models.py
+++ models/models.py
@@ -149,7 +149,7 @@ def appleRecommendationModel(spark, project_id, client):
 
     recommendationModel_table_name_db_path = f"{project_id}.{modelDataset}.{appleRecommendationModel_table_name}"
     sparkDf = read_gbq(spark, cleanDataset, cleanAppleMainScraped_table_name)
-    sparkDf = sparkDf.limit(1000000)
+    sparkDf = sparkDf
 
     # Create "text" column by concatenating title, description, and summary
     sparkDf = sparkDf.withColumn('text', concat(col('name'), lit(' '), col('description')))
@@ -163,7 +163,7 @@ def googleRecommendationModel(spark, project_id, client):
 
     recommendationModel_table_name_db_path = f"{project_id}.{modelDataset}.{googleRecommendationModel_table_name}"
     sparkDf = read_gbq(spark, cleanDataset, cleanGoogleMainScraped_table_name)
-    sparkDf = sparkDf.limit(1000000)
+    sparkDf = sparkDf
 
     # Create "text" column by concatenating title, description, and summary
     sparkDf = sparkDf.withColumn('text', concat(col('title'), lit(' '), col('description'), lit(' '), col('summary')))
